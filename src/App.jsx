6e6f8d70442401f9e37f96843ee050246905b50a
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-10-02 20:22:47 +0200

    code indent cleanup and package version

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Finss from "./pages/Finss";
 
 function App() {
     const theme = useMantineTheme()
-    const { pathname } = useLocation();
+    const {pathname} = useLocation();
 
     const [page, setPage] = useState("Finss")
     /*
@@ -22,9 +22,9 @@ function App() {
     NomDeLaPage identique à pageName spécifié dans la table des liens dans NavigationBar.js
      */
 
-    const isSmallDevice = useMediaQuery('(max-width: '+theme.breakpoints.sm+'px)')
+    const isSmallDevice = useMediaQuery('(max-width: ' + theme.breakpoints.sm + 'px)')
 
-    const navBarWidth= 80
+    const navBarWidth = 80
     const pathWithoutNav = ["/login"]
 
     const withNavBar = (path) => {
@@ -36,35 +36,35 @@ function App() {
         <MantineProvider withGlobalStyles withNormalizeCSS>
 
             {withNavBar(pathname) ? <NavigationBar width={navBarWidth} page={page}/> : ""}
-            <Container fluid // Permet de s'assurer que le container prend la largeur maximale (largeur totale de la page)
-                       style={{
-                margin: "0", //On s'assure qu'il n'y a aucune bordure de type margin ou padding
-                padding: "0",
-                           // Si la navbar est affiché, on met du padding sur le Container pour éviter la superposition
-                           // de la nav bar sur le container
-                marginLeft: (isSmallDevice || !withNavBar(pathname))? 0: navBarWidth,
-                paddingLeft: "0"
-            }}>
+            <Container
+                fluid // Permet de s'assurer que le container prend la largeur maximale (largeur totale de la page)
+                style={{
+                    margin: "0", //On s'assure qu'il n'y a aucune bordure de type margin ou padding
+                    padding: "0",
+                    // Si la navbar est affiché, on met du padding sur le Container pour éviter la superposition
+                    // de la nav bar sur le container
+                    marginLeft: (isSmallDevice || !withNavBar(pathname)) ? 0 : navBarWidth,
+                    paddingLeft: "0"
+                }}>
 
-            <Routes>
-                <Route path="/">
-                    {/*public routes*/}
-                    <Route path="login" element={<Login/>}/>
+                <Routes>
+                    <Route path="/">
+                        {/*public routes*/}
+                        <Route path="login" element={<Login/>}/>
 
-                    {/*privates routes*/}
-                    <Route element={<RequireAuth/>}>
+                        {/*privates routes*/}
+                        <Route element={<RequireAuth/>}>
 
-                        <Route path="/" element={<Home setPage={setPage}/>}/>
-                        <Route path="pg/:pgId" element={<PG setPage={setPage}/>}/>
-                        <Route path="edit" element={<Edit setPage={setPage}/>}/>
-                        <Route path="finss" element={<Finss setPage={setPage}/>}/>
+                            <Route path="/" element={<Home setPage={setPage}/>}/>
+                            <Route path="pg/:pgId" element={<PG setPage={setPage}/>}/>
+                            <Route path="edit" element={<Edit setPage={setPage}/>}/>
+                            <Route path="finss" element={<Finss setPage={setPage}/>}/>
 
+                        </Route>
+
+                        {/* TODO: 404*/}
                     </Route>
-
-                    {/* 404*/}
-                    {/* TODO:*/}
-                </Route>
-            </Routes>
+                </Routes>
             </Container>
 
         </MantineProvider>
