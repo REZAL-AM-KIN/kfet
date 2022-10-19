336df3c44eccf7f4d7ff3778560728c9af30a389
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-10-19 17:44:05 +0200

    qr code scaning big screen

--- src/components/RechargeLydiaButton.js
+++ src/components/RechargeLydiaButton.js
@@ -1,11 +1,21 @@
-import {Button, Modal, Stack, TextInput} from "@mantine/core";
+import {Button, Modal, NumberInput, Stack, TextInput, useMantineTheme} from "@mantine/core";
 import {useState} from "react";
+import QrReader from 'react-qr-reader'
+import errorNotif from "./ErrorNotif";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import {useMediaQuery} from "@mantine/hooks";
+import {IconQrcode} from "@tabler/icons";
 
 function RechargeLydiaButton({pgData, sx}) {
+    const theme = useMantineTheme();
+    const isSmallDevice = useMediaQuery('(max-width: ' + theme.breakpoints.xs + 'px)');
+
+    const axiosPrivate = useAxiosPrivate();
 
     const [qrcode, setQrcode] = useState("");
-
+    const [montant, setMontant] = useState();
     const [opened, setOpened] = useState(false);
+    const [showQrScanner, setShowQrScanner] = useState(false)
 
     /* Function triggered when the recharge button is clicked */
     const modalOpen = () => {
@@ -14,16 +24,49 @@ function RechargeLydiaButton({pgData, sx}) {
 
     const modalClose = () => {
         setOpened(false);
+        setMontant(null);
+        setShowQrScanner(false);
         setQrcode("");
     }
 
-    const handleRechargerSubmit = () => {
-
+    const handleQrSubmit = (e) => {
+        if (e) {
+            e.preventDefault();
+        }
+        const postQr = async () => {
+            try {
+                await axiosPrivate.post("rechargeslydia/",
+                    JSON.stringify({
+                        cible_id: pgData.pgId,
+                        montant: montant,
+                        qrcode: qrcode
+                    }));
+            } catch (error) {
+                errorNotif("QR Submit", error?.response);
+            }
+        }
+        //postQr();
+        // reset the fields
+        errorNotif("qrcode sscanne", "flsdqjfsmlj")
+        modalClose();
     }
 
-    return (
-        <>
-            <Button onClick={modalOpen} sx={sx}>Qr Code</Button>
+    function handleError(error) {
+        errorNotif("Scanning QrCode", error)
+    }
+
+    function handleScan(result) {
+        if (result) {
+            setQrcode(result);
+            setShowQrScanner(false);
+            console.log("RESULTAT", result);
+            // handleQrSubmit();
+        }
+    }
+
+
+    const SmallModal = () => {
+        return (
             <Modal
                 centered
                 opened={opened}
@@ -32,24 +75,97 @@ function RechargeLydiaButton({pgData, sx}) {
                 }}
                 title={"Recharger " + pgData.bucque + " " + pgData.fams}
             >
-                <form onSubmit={handleRechargerSubmit}>
+                <form onSubmit={handleQrSubmit}>
                     <Stack>
-                        <TextInput
+                        <NumberInput
                             data-autofocus
-                            type="text"
-                            placeholder="Scanner le QR Code"
+                            type="number"
+                            hideControls
+                            placeholder="76"
                             label="Montant de la recharge"
-                            value={qrcode}
-                            onChange={(e) => setQrcode(e.currentTarget.value)}
+                            value={montant}
+                            onChange={setMontant}
+                            min={0}
+                            rightSection={<IconQrcode onClick={() => {
+                                setShowQrScanner(!showQrScanner)
+                            }}/>}
+                        />
+                        <QrReader
+                            onError={handleError}
+                            onScan={handleScan}
+                            delay={100}
+                            style={{width: '100%'}}
                         />
                         <Button
+                            disabled={!montant}
+                            onClick={() => {
+                                setShowQrScanner(true)
+                            }}>Scanner</Button>
+                    </Stack>
+                </form>
+            </Modal>
+        );
+    }
+
+    const BigModal = () => {
+        return (
+            <Modal
+                centered
+                opened={opened}
+                onClose={() => {
+                    modalClose()
+                }}
+                title={"Recharger " + pgData.bucque + " " + pgData.fams}
+            >
+                <form onSubmit={handleQrSubmit}>
+                    <Stack>
+                        <NumberInput
+                            data-autofocus
+                            hideControls
+                            type="number"
+                            placeholder="76"
+                            label="Montant de la recharge"
+                            min={0}
+                            value={montant}
+                            onChange={setMontant}
+                            rightSection={<IconQrcode onClick={() => {
+                                    setShowQrScanner(!showQrScanner&&montant);
+                            }}/>
+                            }
+                        />
+                        {showQrScanner
+                            ? <QrReader
+                                onError={handleError}
+                                onScan={handleScan}
+                                delay={100}
+                                style={{width: '100%'}}
+                            />
+                            : <TextInput
+                                type="text"
+                                placeholder="Scanner le QR Code"
+                                label="Montant de la recharge"
+                                disabled={!montant}
+                                value={qrcode}
+                                onChange={(e) => setQrcode(e.currentTarget.value)}
+                            />}
+                        <Button
                             type={"submit"}
-                            disabled={!qrcode}>
+                            disabled={!qrcode || !montant}
+                        >
                             Recharger
                         </Button>
                     </Stack>
                 </form>
             </Modal>
+        )
+            ;
+    }
+
+
+    return (
+        <>
+            <Button onClick={modalOpen} sx={sx}>Qr Code</Button>
+            {isSmallDevice ? <SmallModal/> : <BigModal/>}
         </>
     );
 }
