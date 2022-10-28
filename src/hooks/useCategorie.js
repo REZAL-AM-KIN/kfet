c59876e67b260c6b6030b97ec6ea5e2bc55cad85
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-10-28 21:40:04 +0200

    added color to categorie context

--- src/hooks/useCategorie.js
+++ src/hooks/useCategorie.js
@@ -2,5 +2,9 @@ import {useContext} from "react";
 import CategorieContext from "../context/Categorie";
 
 export function useCategorie() {
-    return useContext(CategorieContext);
+    return useContext(CategorieContext)["categorie"];
+}
+
+export function useCatColor() {
+    return useContext(CategorieContext)["catColor"];
 }
