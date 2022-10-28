acfcf76c577d56619a1030b57ca5d91262b75345
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-10-28 15:33:52 +0200

    categorie and default categorie working

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,30 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 
 function Categories({produits, permissions, value, onChange}) {
 
     let categories = [];
-
-    useEffect(() => {
-        if (Object.keys(permissions).length && !categories.length) {
-            if(categories.length === 0){
-                console.log("longeur de la liste: ", categories.length);
-                console.log('liste: ', categories);
-                console.log("longeur de la liste: (je suis peux etre con) ", categories.length);
-            }
-            for (let line of produits) {
-                if (!categories.includes(line["nom_entite"])) {
-                    if (permissions.all || permissions.groupes.includes(line["nom_entite"])) {
-                        categories.push(line["nom_entite"]);
-                    }
-                }
-            }
-            if (categories.includes(permissions.ipIdentification[0])) {
-                onChange(permissions.ipIdentification[0]);
+    for (let line of produits) {
+        if (!categories.includes(line["nom_entite"])) {
+            if (permissions.all || permissions.groupes.includes(line["nom_entite"])) {
+                categories.push(line["nom_entite"]);
             }
         }
-        // eslint-disable-next-line
-    }, [produits, permissions]);
+    }
 
 
     return (
--- src/context/Categorie.js
+++ src/context/Categorie.js
@@ -1,12 +1,26 @@
-import {createContext, useState} from 'react';
+import {createContext, useEffect, useState} from 'react';
+import {usePermissions} from "../hooks/useUser";
 
 const CategorieContext = createContext({});
 
 export const CategorieProvider = ({children}) => {
     const [categorie, setCategorie] = useState("");
 
+    const permissions = usePermissions();
+
+    useEffect(() => {
+        if (Object.keys(permissions).length) {
+            if(permissions.ipIdentification.length) {
+                setCategorie(permissions.ipIdentification[0]);
+            }
+            else{
+                setCategorie(permissions.groupes[0]);
+            }
+        }
+    }, [permissions])
+
     return (
-        <CategorieContext.Provider value={{categorie, setCategorie}}>
+        <CategorieContext.Provider value={[categorie, setCategorie]}>
             {children}
         </CategorieContext.Provider>
     )
