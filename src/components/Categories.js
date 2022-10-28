df400d2023f84f163e138f4631ff01de29b4df14
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-10-28 18:59:08 +0200

    categorie wip

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,25 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useCategorie} from "../hooks/useCategorie";
+import {IconBoxMultiple} from "@tabler/icons";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import errorNotif from "./ErrorNotif";
+import {Button, Group, Popover} from "@mantine/core";
 
 
-function Categories({produits, permissions, value, onChange}) {
+function Categories({value, onChange}) {
 
-    let categories = [];
-    for (let line of produits) {
-        if (!categories.includes(line["nom_entite"])) {
-            if (permissions.all || permissions.groupes.includes(line["nom_entite"])) {
-                categories.push(line["nom_entite"]);
+    const axiosPrivate = useAxiosPrivate();
+    const [categorie, setCategorie] = useCategorie();
+    const [categories, setCategories] = useState([]);
+    const [catColor, setCatColor] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const getCategories = async () => {
+            try {
+                const response = await axiosPrivate.get("entites/");
+                setCategories(response.data.results);
+                response.data.results.forEach((line) => {
+                    if (line.name === categorie) {
+                        setCatColor(line.color);
+                    }
+                });
+            } catch (error) {
+                console.log(error);
+                errorNotif("Categories", error);
             }
         }
-    }
+        getCategories();
+        return () => {
+            controller.abort();
+        }
+        // eslint-disable-next-line
+    }, [])
 
 
     return (
-        <>
-            <div> shows the categories list {categories.toString()} corresponding to the permissions
-                "{permissions.toString()}"
-            </div>
-            <div> the categorie selected is {value.toString()}</div>
-        </>
+        <Popover
+            width={300}
+            position="right"
+            styles={{
+                dropdown: {
+                    padding: 0,
+                    borderRadius: 9,
+                    borderStyle: "none"
+                }
+            }}
+            shadow="md"
+            offset={20}
+            trapFocus
+        >
+            <Popover.Target>
+                <IconBoxMultiple size={34} stroke={1.5}/>
+            </Popover.Target>
+
+            <Popover.Dropdown>
+                <Group>
+                    {categories.map((cat, key) => {
+                        return (
+                            <Button key={key}
+                                    variant="gradient"
+                                    gradient={{from: catColor, to: cat.color}}
+                                    onClick={() => {
+                                        setCategorie(cat.name);
+                                        setCatColor(cat.color);
+                                    }}
+                            >{cat.name}</Button>
+                        );
+                    })}
+                </Group>
+            </Popover.Dropdown>
+        </Popover>
     );
 }
 
