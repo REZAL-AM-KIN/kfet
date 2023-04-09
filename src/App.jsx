64b6b0d38417f4c5c551324c4a289b6d65f520e8
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2023-04-09 22:53:17 +0200

    mantine notification fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from "./pages/Home";
 import NavigationBar from "./components/NavigationBar";
 import Edit from "./pages/Edit";
 import Finss from "./pages/Finss";
-import {NotificationsProvider} from '@mantine/notifications';
+import {Notifications} from '@mantine/notifications';
 import {UserProvider} from "./context/User";
 
 
@@ -37,41 +37,40 @@ function App() {
 
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
-            <NotificationsProvider>
-                <UserProvider>
-                    {withNavBar(pathname) ? <NavigationBar width={navBarWidth} page={page}/> : ""}
-                    <Container
-                        fluid // Permet de s'assurer que le container prend la largeur maximale (largeur totale de la page)
-                        style={{
-                            margin: "0", //On s'assure qu'il n'y a aucune bordure de type margin ou padding
-                            padding: "0",
-                            // Si la navbar est affiché, on met du padding sur le Container pour éviter la superposition
-                            // de la nav bar sur le container
-                            marginLeft: (isSmallDevice || !withNavBar(pathname)) ? 0 : navBarWidth,
-                            paddingLeft: "0"
-                        }}>
+            <Notifications/>
+            <UserProvider>
+                {withNavBar(pathname) ? <NavigationBar width={navBarWidth} page={page}/> : ""}
+                <Container
+                    fluid // Permet de s'assurer que le container prend la largeur maximale (largeur totale de la page)
+                    style={{
+                        margin: "0", //On s'assure qu'il n'y a aucune bordure de type margin ou padding
+                        padding: "0",
+                        // Si la navbar est affiché, on met du padding sur le Container pour éviter la superposition
+                        // de la nav bar sur le container
+                        marginLeft: (isSmallDevice || !withNavBar(pathname)) ? 0 : navBarWidth,
+                        paddingLeft: "0"
+                    }}>
 
-                        <Routes>
-                            <Route path="/">
-                                {/*public routes*/}
-                                <Route path="login" element={<Login/>}/>
+                    <Routes>
+                        <Route path="/">
+                            {/*public routes*/}
+                            <Route path="login" element={<Login/>}/>
 
-                                {/*privates routes*/}
-                                <Route element={<RequireAuth/>}>
+                            {/*privates routes*/}
+                            <Route element={<RequireAuth/>}>
 
-                                    <Route path="/" element={<Home setPage={setPage}/>}/>
-                                    <Route path="pg/:pgId" element={<PG setPage={setPage}/>}/>
-                                    <Route path="edit" element={<Edit setPage={setPage}/>}/>
-                                    <Route path="finss" element={<Finss setPage={setPage}/>}/>
+                                <Route path="/" element={<Home setPage={setPage}/>}/>
+                                <Route path="pg/:pgId" element={<PG setPage={setPage}/>}/>
+                                <Route path="edit" element={<Edit setPage={setPage}/>}/>
+                                <Route path="finss" element={<Finss setPage={setPage}/>}/>
 
-                                </Route>
-
-                                {/* TODO: 404*/}
                             </Route>
-                        </Routes>
-                    </Container>
-                </UserProvider>
-            </NotificationsProvider>
+
+                            {/* TODO: 404*/}
+                        </Route>
+                    </Routes>
+                </Container>
+            </UserProvider>
         </MantineProvider>
     );
 
