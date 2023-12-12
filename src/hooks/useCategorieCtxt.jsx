cca16f282005f3575294892f66e72d6f016cec23
Rhadamanthe76 <justin.brugiere@gmail.com>
2023-12-12 17:36:21 +0100

    add docstring to useCategorieCtxt

--- src/hooks/useCategorieCtxt.jsx
+++ src/hooks/useCategorieCtxt.jsx
@@ -1,6 +1,19 @@
 import {useContext} from "react";
 import CategorieContext from "../context/Categorie";
 
+/*
+This hooks are used to get the categorie and the color of the categorie
+from the context
+
+useCategorieCtxt() ->
+    categorie: str, name of the categorie
+    setCategorie
+
+useCatColor() ->
+    catColor:str, color of the categorie (hex) '#aabbcc'
+    setCatColor
+*/
+
 export function useCategorieCtxt() {
     return useContext(CategorieContext)["categorie"];
 }
